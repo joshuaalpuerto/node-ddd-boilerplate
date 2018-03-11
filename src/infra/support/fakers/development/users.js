2a109735d0c66ed7e2f9c705c852528287d41cb7
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-03-11 13:43:10 +0800

    Created seperate user faker

---
+++ src/infra/support/fakers/development/users.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt')
+
+module.exports = () => {
+  const salt = bcrypt.genSaltSync()
+  const password = bcrypt.hashSync('pass', salt)
+
+  return [{
+    id: '48e40a9c-c5e9-4d63-9aba-b77cdf4ca67b',
+    firstName: 'Test',
+    lastName: 'Developer',
+    middleName: 'Super Dev',
+    email: 'testdev@gmail.com',
+    password: password,
+    roleId: 1,
+    verificationCode: 'ba1bfda5-1c27-4755-bd23-36c7a4dbfd2b',
+    isVerified: 1,
+    isDeleted: 0,
+    createdBy: '48e40a9c-c5e9-4d63-9aba-b77cdf4ca67b'
+  }]
+}
