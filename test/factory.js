332d4e563312629e8a66a9e92d35f782ac2f260d
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-05-22 14:19:16 +0800

    refactor(Test Factory): Simplified fetching of repo and model

--- test/factory.js
+++ test/factory.js
@@ -1,15 +1,17 @@
 /**
  * We need to find a way to user the repositories for our test that it we will have minimum impact once we change our ORM or our DATABASE
  */
-const { compose } = require('ramda')
+const { curry } = require('ramda')
 
  // we will call each repo on thier test  cases  here we will just compose the items.
 
-const repository = (model) => {
-  const entity = app.resolve('database').models[model]
-  return (repo) => compose(repo)(entity)
-}
+const models = (name) => app.resolve('database').models[name]
+
+const repository = curry((repo, model) => {
+  return repo(model)
+})
 
 module.exports = {
+  models,
   repository
 }
