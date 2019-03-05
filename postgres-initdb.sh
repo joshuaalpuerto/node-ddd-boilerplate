50449915c747959f50fb72cad916c231658279c3
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-03-05 22:45:54 +0800

    Fix initialization postgres

--- postgres-initdb.sh
+++ postgres-initdb.sh
@@ -8,12 +8,12 @@ psql --variable=ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE DATABASE "node_ddd_test";
 EOSQL
 
-psql --variable=ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=reforecast <<-EOSQL
+psql --variable=ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=node_ddd <<-EOSQL
   CREATE EXTENSION "uuid-ossp";
   CREATE EXTENSION "hstore";
 EOSQL
 
-psql --variable=ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=reforecast_test <<-EOSQL
+psql --variable=ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=node_ddd_test <<-EOSQL
   CREATE EXTENSION "uuid-ossp";
   CREATE EXTENSION "hstore";
 EOSQL
