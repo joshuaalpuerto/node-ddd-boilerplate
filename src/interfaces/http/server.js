8f17c0522af06ef19003ca49d347dcc04f1be729
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2020-02-16 17:31:10 +0200

    Expose healthcheck endpoint for server

--- src/interfaces/http/server.js
+++ src/interfaces/http/server.js
@@ -1,16 +1,6 @@
 const express = require('express')
-const { createTerminus, HealthCheckError } = require('@godaddy/terminus')
 
-// Healthchecks
-const readinessProbe = ({ http, database }) => async () => {
-  const isServerListening = http.listening
-  const isDBConnected = await database.authenticate()
-  if (!isDBConnected || !isServerListening) {
-    throw HealthCheckError
-  }
-}
-
-module.exports = ({ config, router, logger, auth, database }) => {
+module.exports = ({ config, router, logger, auth, database, health }) => {
   const app = express()
 
   app.disable('x-powered-by')
@@ -22,17 +12,12 @@ module.exports = ({ config, router, logger, auth, database }) => {
 
   return {
     app,
+    setupHealthCheck: () =>
+      new Promise(resolve => {
+        resolve(health.start(app))
+      }),
     start: () =>
       new Promise(resolve => {
-        const terminusOptions = {
-          healthChecks: {
-            '/livez': () => {},
-            '/readyz': readinessProbe({ app, database })
-          }
-        }
-
-        createTerminus(app, terminusOptions)
-
         const http = app.listen(config.port, () => {
           const { port } = http.address()
           logger.info(`🤘 API - Port ${port}`)
