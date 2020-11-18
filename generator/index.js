13cdb8e5e81090885bb5c7ce337ffb9363e6f997
Sofyrus <contact@sofyrus.com>
2020-11-19 00:57:51 +0530

    remove domain generator ref

--- generator/index.js
+++ generator/index.js
@@ -1,11 +1,9 @@
-const appGenerator = require('./app/index.js');
-const repositoryGenerator = require('./domain/index.js');
-const routeGenerator = require('./route/index.js');
-const domainGenerator = require('./repository/index.js');
+const appGenerator = require("./app/index.js");
+const routeGenerator = require("./route/index.js");
+const repositoryGenerator = require("./repository/index.js");
 
 module.exports = (plop) => {
-    plop.setGenerator('app', appGenerator);
-    plop.setGenerator('repository', repositoryGenerator);
-    plop.setGenerator('route', routeGenerator);
-    plop.setGenerator('domain', domainGenerator);
+  plop.setGenerator("app", appGenerator);
+  plop.setGenerator("repository", repositoryGenerator);
+  plop.setGenerator("route", routeGenerator);
 };
