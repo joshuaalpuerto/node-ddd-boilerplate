0c6f48046fe71c260e7bc30119bb4efe667e152b
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2025-05-02 09:05:03 +0300

    Handle gracefully exit

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ app
     app.logger.error(error.stack)
     process.exit()
   })
+
+
+
+function exitHandler(exitCode = 0) {
+  app.close();
+  app.logger.info('Exiting with code:', exitCode);
+  process.exit(exitCode);
+}
+
+const unexpectedErrorHandler = (error) => {
+  app.logger.error(error);
+  exitHandler(1);
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+process.on('SIGTERM', (_, code) => exitHandler(code));
+process.on('SIGINT', (_, code) => exitHandler(code));
